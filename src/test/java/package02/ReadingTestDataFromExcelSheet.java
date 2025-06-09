5ef8dd8fa37a6733760cca87aa72b76d87cdf519
umesh.ukv1392@gmail.com <umesh.ukv131992@gmail.com>
2025-06-10 00:27:16 +0530

    added read the data from excel sheet

--- src/test/java/package02/ReadingTestDataFromExcelSheet.java
+++ src/test/java/package02/ReadingTestDataFromExcelSheet.java
@@ -2,31 +2,69 @@ package package02;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 
 public class ReadingTestDataFromExcelSheet {
 
-	public static Object[][]getTestDataFromExcel(String sheetName)
+
+	@Test(dataProvider="CredentialsSupplier")
+	public void loginTest(String username, String password)
+	{
+		System.out.println(username  + "------------------ "+ password);
+	}
+
+	@DataProvider(name="CredentialsSupplier")
+	public Object[][] dataSupplier() throws IOException
 	{
 
-		File excelFile = new File(System.getProperty("user.dir"));
-		XSSFWorkbook workbook= null;
-		
-		try
+		File excelFile = new File(System.getProperty("user.dir")+"\\testdata01.xlsx");
+		FileInputStream fisFile = new FileInputStream(excelFile);
+		XSSFWorkbook workbook = new XSSFWorkbook(fisFile);
+		XSSFSheet sheet = workbook.getSheet("loginpage");
+
+		int rowCount = sheet.getLastRowNum();
+		int coulmnCount = sheet.getRow(0).getLastCellNum();
+
+		Object[][] data = new Object[rowCount][coulmnCount];
+
+		for(int r=0; r<rowCount; r++)
 		{
-			FileInputStream fisFile = new  FileInputStream(excelFile);
-			workbook = new XSSFWorkbook(fisFile);	
+			XSSFRow row = sheet.getRow(r+1);
+
+			for(int c=0; c<coulmnCount; c++)
+			{
+
+				XSSFCell cell = row.getCell(c);
+
+				CellType cellType = cell.getCellType();
+
+				switch(cellType)
+				{
+				case STRING:
+					data [r][c] = cell.getStringCellValue();
+					break;
+
+				case NUMERIC:
+					data[r][c] = Integer.toString((int)cell.getNumericCellValue());
+					break;
+
+				case BOOLEAN:
+					data[r][c] = cell.getBooleanCellValue();
+					break;
+				}
+			} 
 		}
 		
-		catch(Throwable e)
-		{
-			e.printStackTrace();
-		}
-		
-		
-
-
-
-
-
-	}
+	return data;
+	}	
 }
+
